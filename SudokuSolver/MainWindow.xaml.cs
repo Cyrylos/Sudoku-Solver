31fca42fdc0a00401632a4ad50cdc080eef5c92a
Cyryl Wilczek <cyrylw@gmail.com>
2021-08-01 23:26:03 +0200

    Dodaj pliki projektów.

---
+++ SudokuSolver/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolverWPF
+{
+    public partial class MainWindow : Window
+    {
+        private TextBox[,] sudokuFields = new TextBox[9, 9];
+        private int[,] sudokuBoard = new int[9, 9];
+        public MainWindow()
+        {
+            InitializeComponent();
+            sudokuFields = new TextBox[,] { { Field00, Field01, Field02, Field03, Field04, Field05, Field06, Field07, Field08},
+                { Field10, Field11, Field12, Field13, Field14, Field15, Field16, Field17, Field18},
+                { Field20, Field21, Field22, Field23, Field24, Field25, Field26, Field27, Field28},
+                { Field30, Field31, Field32, Field33, Field34, Field35, Field36, Field37, Field38},
+                { Field40, Field41, Field42, Field43, Field44, Field45, Field46, Field47, Field48},
+                { Field50, Field51, Field52, Field53, Field54, Field55, Field56, Field57, Field58},
+                { Field60, Field61, Field62, Field63, Field64, Field65, Field66, Field67, Field68},
+                { Field70, Field71, Field72, Field73, Field74, Field75, Field76, Field77, Field78},
+                { Field80, Field81, Field82, Field83, Field84, Field85, Field86, Field87, Field88} };
+        }
+
+        private void Solve_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    String sudokuFieldValue = sudokuFields[i, j].Text;
+                    if (sudokuFieldValue.Equals(""))
+                    {
+                        sudokuBoard[i, j] = 0;
+                    }
+                    else
+                    {
+                        sudokuBoard[i, j] = int.Parse(sudokuFieldValue);
+                    }
+                }
+            }
+
+            SudokuSolver sudokusolver = new SudokuSolver(sudokuBoard);
+            if (!sudokusolver.IsConsistent())
+            {
+                MessageBox.Show("Sudoku board isn't consistent!");
+                return;
+            }
+            if (!sudokusolver.Solve())
+            {
+                MessageBox.Show("This sudoku doesn't have a sulution!");
+                return;
+            }
+            sudokuBoard = sudokusolver.SudokuBoard;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    String sudokuFieldValue = sudokuFields[i, j].Text;
+                    if (sudokuFieldValue.Equals(""))
+                    {
+                        sudokuFields[i, j].Text = sudokuBoard[i, j].ToString();
+                    }
+                    else
+                    {
+                        sudokuFields[i, j].Background = new SolidColorBrush(Colors.LightGray);
+                    }
+                }
+            }
+        }
+
+        private void ClearBoard_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudokuFields[i, j].Background = new SolidColorBrush(Colors.White);
+                    sudokuFields[i, j].Clear();
+                    sudokuBoard[i, j] = 0;
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void IsNumber_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            String fieldValue = ((TextBox)sender).Text + e.Text;
+            Regex reg = new Regex("^[1-9]$");
+            e.Handled = !reg.IsMatch(fieldValue);
+        }
+    }
+}
---
+++ SudokuSolver/SudokuSolver.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolverWPF
+{
+    class SudokuSolver
+    {
+        private int[,] sudokuBoard = new int[9, 9];
+
+        public int[,] SudokuBoard { get => sudokuBoard; set => sudokuBoard = value; }
+
+        public SudokuSolver(int[,] sudokuBoard)
+        {
+            this.SudokuBoard = sudokuBoard;
+        }
+
+        public bool IsConsistent()
+        {
+            int rows = SudokuBoard.GetLength(0);
+            int columns = SudokuBoard.GetLength(1);
+            if (rows != 9 || columns != 9)
+            {
+                return false;
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (!IsColumnConsistent(i) || !IsRowConsistent(i) || !IsSubgridConsistent(i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsRowConsistent(int row)
+        {
+            List<int> rowValues = new List<int>();
+            for (int i = 0; i < 9; i++)
+            {
+                int currentValue = SudokuBoard[row, i];
+                if (currentValue != 0)
+                {
+                    if (rowValues.Contains(currentValue))
+                    {
+                        return false;
+                    }
+                    rowValues.Add(currentValue);
+                }
+            }
+            return true;
+        }
+
+        private bool IsColumnConsistent(int column)
+        {
+            List<int> rowValues = new List<int>();
+            for (int i = 0; i < 9; i++)
+            {
+                int currentValue = SudokuBoard[i, column];
+                if (currentValue != 0)
+                {
+                    if (rowValues.Contains(currentValue))
+                    {
+                        return false;
+                    }
+                    rowValues.Add(currentValue);
+                }
+            }
+            return true;
+        }
+
+        private bool IsSubgridConsistent(int subgrid)
+        {
+            int firstX = (subgrid % 3) * 3;
+            int firstY = (subgrid / 3) * 3;
+            List<int> subgridValues = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int x = firstX + i;
+                    int y = firstY + j;
+                    int currentValue = SudokuBoard[y, x];
+                    if (currentValue != 0)
+                    {
+                        if (subgridValues.Contains(currentValue))
+                        {
+                            return false;
+                        }
+                        subgridValues.Add(currentValue);
+                    }
+                }
+            }
+            return true;
+        }
+
+        private List<int> AvailableValues(int x, int y)
+        {
+            List<int> availableValues = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int firstSubgridX = (x / 3) * 3;
+            int firstSubgridY = (y / 3) * 3;
+            //removing values from row and column
+            for (int i = 0; i < 9; i++)
+            {
+                availableValues.Remove(SudokuBoard[y, i]);
+                availableValues.Remove(SudokuBoard[i, x]);
+            }
+
+            //removing values from current subgrid
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int currentX = firstSubgridX + i;
+                    int currentY = firstSubgridY + j;
+                    int currentValue = sudokuBoard[currentY, currentX];
+                    if (currentValue != 0)
+                    {
+                        availableValues.Remove(currentValue);
+                    }
+                }
+            }
+
+            return availableValues;
+        }
+
+        public bool Solve()
+        {
+            //int x, y;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int currentValue = SudokuBoard[i, j];
+                    if (currentValue == 0)
+                    {
+                        var availableValues = AvailableValues(j, i);
+                        foreach (int value in availableValues)
+                        {
+                            sudokuBoard[i, j] = value;
+                            if (Solve())
+                            {
+                                return true;
+                            }
+                        }
+                        // no solution in this path - rollback
+                        sudokuBoard[i, j] = 0;
+                        return false;
+                    }
+                }
+            }
+            // no empty values
+            return true;
+        }
+
+
+
+    }
+}
